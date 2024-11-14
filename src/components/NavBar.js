5e9e8a9240db518964acffb213c0a351a9c92ca6
MonetteParangat <monette.parangat@ibm.com>
2024-11-15 01:29:20 +0800

    initial commit Navbar.js, NavBar.css and added Navbar to App.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,22 @@
-function NavBar(){
-    return(
-        <div>
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import '../style/NavBar.css'
 
-        </div>
+function NavBar() {
+    return (
+        <Navbar className="navbar" expand="lg">
+            <Container>
+                <Navbar.Brand href="#home">My Portfolio</Navbar.Brand>
+                <Navbar.Toggle aria-controls="navbar-nav" />
+                <Navbar.Collapse id="navbar-nav">
+                    <Nav className="ms-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#about">About</Nav.Link>
+                        <Nav.Link href="#projects">Projects</Nav.Link>
+                        <Nav.Link href="#contact">Contact</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     );
 }
 
