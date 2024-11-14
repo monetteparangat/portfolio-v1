8639a780b5bfddb32f4bdd9da12dfde034b4224e
MonetteParangat <monette.parangat@ibm.com>
2024-11-15 04:24:10 +0800

    Initial commit banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,7 +1,25 @@
-function Banner(){
-    return(
-        <section id="home">
+import '../style/Banner.css'
+import walkImg from '../assets/img/walk.svg'
+import flower from '../assets/img/flower.jpg'
+import welcome from '../assets/img/welcome.svg'
 
+function Banner() {
+    return (
+        <section id="home">
+            <div className="container-banner">
+                <div className='row'>
+                    <div className='col'>
+                        <span>{'Welcome! I\’m glad you’re here.'}</span>
+                        <h1>{'I\'m Monette'} </h1>
+                        <h2>WEB DEVELOPER</h2>
+                        <p>{'I\'m a developer who loves building interactive web experiences. From React front-end to back-end systems, I enjoy turning ideas into clean, efficient code. Always learning and solving new problems along the way!'}</p>
+                        <button>Connect</button>
+                    </div>
+                    <div className='col'>
+                        <img src={welcome} alt='walk' />
+                    </div>
+                </div>
+            </div>
         </section>
     );
 }
